036d1bfd029eec19170b4f96c627694be4596e74
weyne <weyneace@gmail.com>
2023-07-20 21:05:52 -0300

    Create LancamentoService.js

---
+++ client/services/LancamentoService.js
@@ -0,0 +1,16 @@
+class LancamentoService {
+  constructor(httpClient, baseUrl) {
+    this.httpClient = httpClient
+    this.baseUrl = baseUrl
+  }
+
+  async getLancamentos() {
+    return this.httpClient.get(`${this.baseUrl}/api/lancamentos`)
+  }
+  async saveLancamento(lancamento) {
+    return this.httpClient.post(`${this.baseUrl}/api/lancamentos`, lancamento)
+  }
+  async deleteLancamento(id) {
+    return this.httpClient.delete(`${this.baseUrl}/api/lancamentos/${id}`)
+  }
+}
